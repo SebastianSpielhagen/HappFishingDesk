d87f7fabcba4c413541974d7d03d179dc0ca9bc9
Sebastian Spielhagen <149331170+SebastianSpielhagen@users.noreply.github.com>
2024-02-02 02:34:39 +0100

    fix intro

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -10,22 +10,35 @@ const HomePage: React.FC = () => {
                 <div className="homepage-text">
                     <h5>Happy Fishing Desk</h5>
                     <p>Die Mitgliederverwaltung für Angelvereine. Erleichtern Sie die Verwaltung Ihrer Mitglieder.</p>
-                    <li>Formulare für die Anmeldung und Erlaubnisscheine an einem Platz und ausgefüllt für die jeweilige Person.</li>
-                    <li>Behalten Sie den Überblick über Ihre Mitglieder und verwalten Sie Ihre diese einfach und an einem Ort.</li>
-                    <li>Verwalten Sie den Kalender für alle Mitglieder. Diese können sich mittels der Happy Fishing Mobile - App verbinden und sehen somit alle anstehenden Veranstaltungen.</li>
-                    <li>Pushen Sie Nachrichten direkt an Ihre Mitglieder. Gratulieren Sie zum Geburtstag, zum Jubiläum, wichtige Nachrichten, Erinnerungen, usw.</li>
-                    <li>Verwalten Sie die Fangberichte Ihrer Mitglieder. Diese können direkt in der Happy Fishing Mobile - App eingetragen werden.</li>
+                    <li>Formulare für die Anmeldung und Erlaubnisscheine an einem Platz und ausgefüllt für die
+                        jeweilige Person.</li>
+                    <li>Behalten Sie den Überblick über Ihre Mitglieder und verwalten Sie Ihre diese einfach und an
+                        einem Ort.</li>
+                    <li>Verwalten Sie den Kalender für alle Mitglieder. Diese können sich mittels der Happy Fishing
+                        Mobile - App verbinden und sehen somit alle anstehenden Veranstaltungen.</li>
+                    <li>Pushen Sie Nachrichten direkt an Ihre Mitglieder. Gratulieren Sie zum Geburtstag, zum Jubiläum,
+                        wichtige Nachrichten, Erinnerungen, usw.</li>
+                    <li>Verwalten Sie die Fangberichte Ihrer Mitglieder. Diese können direkt in der Happy Fishing
+                        Mobile - App eingetragen werden.</li>
                     <li>Erstellen Sie Statistiken über die Fänge Ihrer Mitglieder.</li>
                     <li>Automatische Erinnerung 4 Wochen bevor der Angelschein abläuft.</li>
                     <li>Sperren von Gewässerabschnitten oder komplett mit direkter Meldung an die Mitglieder.</li>
-                    <li>Verwalten Sie die Mitgliederbeiträge und sehen Sie auf einem Blick wer noch nicht bezahlt hat.</li>
-                    <li>Erstellen Sie die Erlaubnisscheine für die Mitglieder oder Gäste und pushen diese direkt an die Happy Fishing Mobile App.</li>
-                    <li>Sperren Sie die Erlaubnisscheine bei nicht bezahlter Rechnung oder Beendigung der Mitgliedschaft.</li>
-                    <li>Gewässerkontrolleure sehen auf einem Blick auf das Handy in der Happy Fishing Mobile App ob alles OK ist.</li>
-                    <li>Verwalten Sie für die Mitglieder die geleisteten Arbeitsstunden, die jedes Mitglied über die Happy Fishing Mobile App einsehen kann.</li>
-                    <li>Sortierbare Mitgliederliste nach mehreren Filtern und drucken Sie sich diese Liste komplett aus für eine Übersicht</li>
-                    <li>Beim Anlegen von neuen Mitgliedern wird der Angelschein in die Happy Fishing Desk hochgeladen inkl. Foto des Mitgliedes.</li><br/>
-                    <p>.... und noch viele Dinge mehr, die das zentrale Verwalten eines Angelvereins vereinfacht und eine Menge Zeit spart</p>
+                    <li>Verwalten Sie die Mitgliederbeiträge und sehen Sie auf einem Blick wer noch nicht bezahlt
+                        hat.</li>
+                    <li>Erstellen Sie die Erlaubnisscheine für die Mitglieder oder Gäste und pushen diese direkt an
+                        die Happy Fishing Mobile App.</li>
+                    <li>Sperren Sie die Erlaubnisscheine bei nicht bezahlter Rechnung oder Beendigung der
+                        Mitgliedschaft.</li>
+                    <li>Gewässerkontrolleure sehen auf einem Blick auf das Handy in der Happy Fishing Mobile
+                        App ob alles OK ist.</li>
+                    <li>Verwalten Sie für die Mitglieder die geleisteten Arbeitsstunden, die jedes Mitglied über die
+                        Happy Fishing Mobile App einsehen kann.</li>
+                    <li>Sortierbare Mitgliederliste nach mehreren Filtern und drucken Sie sich diese Liste komplett aus
+                        für eine Übersicht</li>
+                    <li>Beim Anlegen von neuen Mitgliedern wird der Angelschein in die Happy Fishing Desk hochgeladen
+                        inkl. Foto des Mitgliedes.</li><br/>
+                    <p>.... und noch viele Dinge mehr, die das zentrale Verwalten eines Angelvereins vereinfacht und
+                        eine Menge Zeit spart</p>
                 </div>
 
             </div>
